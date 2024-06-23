05d3300e55ddaf16a60c4b9db757dd490e3eef80
Kin1599 <dkonopliannikov05@mail.ru>
2024-06-23 23:46:06 +0300

    add filters in MyReportsPage

--- src/UI/YellowBtn/YellowBtn.jsx
+++ src/UI/YellowBtn/YellowBtn.jsx
@@ -3,7 +3,7 @@ import cl from './YellowBtn.module.css'
 
 function YellowBtn({onClick, children}) {
   return (
-    <div className={cl.yellowBtn_wrapper}>
+    <div className={cl.yellowBtn_wrapper} onClick={() => onClick()}>
         <button className={cl.yellowBtn}>{children}</button>
     </div>
   )
--- src/pages/MyReportsPage/MyReportPage.jsx
+++ src/pages/MyReportsPage/MyReportPage.jsx
@@ -4,6 +4,8 @@ import Header from '../../components/Header/Header'
 import SearchInput from '../../components/SearchInput/SearchInput'
 import YellowBtn from '../../UI/YellowBtn/YellowBtn'
 import MyReportComponent from '../../components/MyReportComponent/MyReportComponent'
+import WhiteBtn from '../../UI/WhiteBtn/WhiteBtn'
+import FilterComponent from '../../components/FilterComponent/FilterComponent'
 
 function MyReportPage() {
     const [reports] = useState([
@@ -14,6 +16,63 @@ function MyReportPage() {
         {title: 'Сравнение Apple и Samsung', date: '24 дня назад'},
         {title: 'Анализ конкурентов Северстали', date: '2 месяца назад'},
     ])
+
+    const [isFilters, setIsFilters] = useState(false);
+    const [filters, setFilters] = useState([
+        {title: 'Период', options: [
+            {id: 0, text: 'Последний час'},
+            {id: 1, text: 'Последние 12 часов'},
+            {id: 2, text: 'Последние сутки'},
+            {id: 3, text: 'Последние 3 дня'},
+            {id: 4, text: 'Последняя неделя'},
+            {id: 5, text: 'Последний месяц'},
+            {id: 6, text: 'Последний год'},
+        ]},
+        {title: 'LLM модель', options: [
+            {id: 0, text: 'ChatGPT'},
+            {id: 1, text: 'ChatGPT'},
+        ]},
+        {title: 'Тематика отчета', options: [
+            {id: 0, text: 'Рыночные анализы'},
+            {id: 1, text: 'Финансовый анализ'},
+            {id: 2, text: 'Сравнение продуктов'},
+            {id: 3, text: 'Сравнение технологий'},
+            {id: 4, text: 'Анализ конкурентов'},
+        ]}
+    ])
+
+    const initializeSelectedFilters = () => {
+        const initialFilters = {};
+        filters.forEach(filter => {
+            initialFilters[filter.title] = [];
+        });
+        return initialFilters;
+    };
+    
+    const [selectedFilters, setSelectedFilters] = useState(initializeSelectedFilters());
+
+    const closeFilters = () => {
+        setIsFilters(false);
+    }
+
+    const openFilters = () => {
+        setIsFilters(true);
+    }
+
+    const handleFilterChange = (filterTitle, optionText) => {
+        setSelectedFilters(prevState => {
+            const filterOptions = prevState[filterTitle] || [];
+            const isSelected = filterOptions.includes(optionText);
+    
+            return {
+                ...prevState,
+                [filterTitle]: isSelected
+                    ? filterOptions.filter(option => option !== optionText)
+                    : [...filterOptions, optionText]
+            };
+        });
+    };
+
   return (
     <>
         <Header/>
@@ -22,16 +81,39 @@ function MyReportPage() {
             <div className={cl.myReportPage__content}>
                 <div className={cl.content_search}>
                     <SearchInput placeholder="Поиск"/>
-                    <YellowBtn>Фильтр</YellowBtn>
+                    {
+                        isFilters 
+                        ?
+                        <WhiteBtn onClick={closeFilters}>Скрыть</WhiteBtn>
+                        :
+                        <YellowBtn onClick={openFilters}>Фильтр</YellowBtn>
+                    }
                 </div>
+                {
+                    isFilters && (
+                        <div className={cl.content__filters}>
+                            {
+                                filters && filters.map((filter, index) => (
+                                    <FilterComponent 
+                                        title={filter.title} 
+                                        options={filter.options}
+                                        selectedOptions={selectedFilters[filter.title]}
+                                        onFilterChange={handleFilterChange}
+                                    />
+                                ))
+                            }
+                        </div>
+                    )
+                }
+                
                 <div className={cl.content_reports}>
                     {
                         reports 
                         ?
                         <div className={cl.reports}>
                             {
-                                reports.map((report) => (
-                                    <MyReportComponent title={report.title} date={report.date}/>
+                                reports.map((report, index) => (
+                                    <MyReportComponent key={index} title={report.title} date={report.date}/>
                                 ))
                             }
                         </div>
