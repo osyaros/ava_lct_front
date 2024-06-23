d916306c6d735870a6ca5dabd7d2c937a0811a2e
Kin1599 <dkonopliannikov05@mail.ru>
2024-06-24 01:22:19 +0300

    add ProfilePage

--- src/UI/YellowBtn/YellowBtn.jsx
+++ src/UI/YellowBtn/YellowBtn.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import cl from './YellowBtn.module.css'
 
-function YellowBtn({onClick, children}) {
+function YellowBtn({onClick, children, width}) {
   return (
     <div className={cl.yellowBtn_wrapper} onClick={() => onClick()}>
-        <button className={cl.yellowBtn}>{children}</button>
+        <button className={cl.yellowBtn} style={{ maxWidth: width}}>{children}</button>
     </div>
   )
 }
---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Header from '../../components/Header/Header'
+import cl from './ProfilePage.module.css'
+import exit from '../../assets/images/exit.svg'
+import ProfileField from '../../components/ProfileField/ProfileField'
+import YellowBtn from '../../UI/YellowBtn/YellowBtn'
+import profile from '../../assets/images/profile.svg'
+function ProfilePage() {
+    const [fieldsChanged, setFieldsChanged] = useState({
+      name: false,
+      login: false,
+      password: false,
+    });
+
+    const handleFieldChange = (field, changed) => {
+      setFieldsChanged((prevFields) => ({
+        ...prevFields,
+        [field]: changed,
+      }));
+    };
+
+    const isChanged = Object.values(fieldsChanged).some((changed) => changed);
+ 
+  return (
+    <>
+        <Header />
+        <div className={cl.profilePage}>
+            <div className={cl.profilePage__header}>
+                <div className={cl.header_title}>Мой профиль</div>
+                <div className={cl.header_exit}>
+                    <img src={exit} alt="exit" />
+                    Выйти
+                </div>
+            </div>
+            <div className={cl.profilePage__content}>
+                <div className={cl.content__profile}>
+                    <div className={cl.profile_view}>
+                        <div className={cl.view_image}>
+                            <img src={profile} alt="profile"/>
+                        </div>
+                        <div className={cl.view_btn}>
+                            <YellowBtn>Изменить фото</YellowBtn>
+                        </div>
+                    </div>
+                    <div className={cl.profile_fields}>
+                        <ProfileField title="Имя" text="Иван" onChange={(changed) => handleFieldChange('name', changed)}/>
+                        <ProfileField title="Логин" text="ivan1998" onChange={(changed) => handleFieldChange('login', changed)}/>
+                        <ProfileField title="Пароль" text="**********" onChange={(changed) => handleFieldChange('password', changed)}/>
+                        {
+                            isChanged && <YellowBtn width="100%">Подтвердить</YellowBtn>
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ProfilePage
